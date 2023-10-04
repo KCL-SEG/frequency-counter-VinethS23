34f69f7c0518c13f7497ceb1a2105c3312f4916f
Vineth Siriwardana <VASiriwardana@gmail.com>
2023-10-04 13:30:58 +0100

    Created the working frequency counter

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in items:
+        currentItem = i
+        if i in frequencies: 
+            frequencies[str(currentItem)] += 1 
+        else:
+            frequencies[str(currentItem)] = 1
     return frequencies
+# return call is the last statement of a function
+
+#print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))
